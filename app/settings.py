ce9ddb338037b866bef25933a49dea486268fee4
Francis J. García <contacto@francisjgarcia.es>
2022-02-06 18:24:34 +0100

    Delete .json file and logs and add specific user agent

--- app/settings.py
+++ app/settings.py
@@ -8,8 +8,7 @@ todoist_token = config['TODOIST_TOKEN']
 
 # Scrapy general settings
 scrapy_settings = {
-    "FEEDS": {
-        "items.json": {"format": "json"},
-    },
     'FEED_EXPORT_ENCODING': 'utf-8',
+    'LOG_ENABLED': 'False',
+    'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:96.0) Gecko/20100101 Firefox/96.0'
 }
