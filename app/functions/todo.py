dc989736f6152f2d94ad2ce6892a7d4cc9a077da
Francis J. García <contacto@francisjgarcia.es>
2022-05-22 17:53:04 +0200

    Convert individual json to json list

--- app/functions/todo.py
+++ app/functions/todo.py
@@ -1,5 +1,4 @@
 
-import json
 from todoist import TodoistAPI
 from settings import todoist_token
 
@@ -12,14 +11,13 @@ def getProjectId(project_name):
     project_id = list(filter(lambda x:x["name"]==project_name, api.state['projects']))
     return(project_id[0]['id'])
 
-def addTask(local, visitor, competition, date, hour, priority, channel):
+def addTask(local, visitor, competition, date, hour, priority, channel, matches):
     try:
         checktask = list(filter(lambda x:x["due"]["date"]==date+"T"+hour+":00", list(filter(lambda x:x["content"]==local+' - '+visitor, api.state['items']))))
         if not checktask:
             api.items.add(content=local+' - '+visitor, project_id=getProjectId(competition), due={"date": date+"T"+hour+":00"}, priority=priority, description=channel)
-            json_task = '{"local": "'+local+'", "visitor": "'+visitor+'", "competition": "'+competition+'", "date": "'+date+'", "hour": "'+hour+'", "channel": "'+channel+'"}'
-            json_parsed = json.loads(json_task)
-            print(json.dumps(json_parsed, indent=2, ensure_ascii=False))
+            json_task = {"local": ""+local+"", "visitor": ""+visitor+"", "competition": ""+competition+"", "date": ""+date+"", "hour": ""+hour.strip()+"", "channel": ""+channel+""}
+            matches.append(json_task)
             api.commit()
     except:
         print("No se ha podido añadir el siguiente partido: "+local, visitor, competition, date, hour, priority, channel)
