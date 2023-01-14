8a9e6b843f90729ba91b763a99cf8db9c31be171
Francis J. García <contacto@francisjgarcia.es>
2023-01-14 19:02:17 +0100

    Change Todoist API package

--- app/functions/todo.py
+++ app/functions/todo.py
@@ -1,23 +1,31 @@
 
-from todoist import TodoistAPI
+from todoist_api_python.api import TodoistAPI
 from settings import todoist_token
+import re
 
 api = TodoistAPI(todoist_token)
-api.sync()
 
 def getProjectId(project_name):
     if project_name == "null":
         project_name = "Fútbol"
-    project_id = list(filter(lambda x:x["name"]==project_name, api.state['projects']))
-    return(project_id[0]['id'])
+    for project in api.get_projects():
+        if re.search(project_name, str(project)):
+            regex = re.findall(" id='[0-9]+'", str(project))
+            for id in regex:
+                return(id.strip().split("'")[1])
+
+def getTaskId(task_name):
+    for task in api.get_tasks():
+        if re.search(task_name, str(task)):
+            regex = re.findall(" id='[0-9]+'", str(task))
+            for id in regex:
+                return(id.strip().split("'")[1])
 
 def addTask(local, visitor, competition, date, hour, priority, channel, matches):
     try:
-        checktask = list(filter(lambda x:x["due"]["date"]==date+"T"+hour+":00", list(filter(lambda x:x["content"]==local+' - '+visitor, api.state['items']))))
-        if not checktask:
-            api.items.add(content=local+' - '+visitor, project_id=getProjectId(competition), due={"date": date+"T"+hour+":00"}, priority=priority, description=channel)
+        if not getTaskId(local+' - '+visitor):
+            api.add_task(content=local+' - '+visitor, project_id=getProjectId(competition), due_date=date+"T"+hour+":00", priority=priority, description=channel)
             json_task = {"local": ""+local+"", "visitor": ""+visitor+"", "competition": ""+competition+"", "date": ""+date+"", "hour": ""+hour.strip()+"", "channel": ""+channel+""}
             matches.append(json_task)
-            api.commit()
     except:
         print("No se ha podido añadir el siguiente partido: "+local, visitor, competition, date, hour, priority, channel)
